4d36eaf5b69801b219a71205c096a784636964b1
Medrine-Jepkemoi <99050771+Medrine-Jepkemoi@users.noreply.github.com>
2023-11-19 22:39:46 +0300

    Structure Setup

---
+++ src/Components/search.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Search = () => 
+{
+    return(
+        <div>
+            <input name="Search" placeholder="Enter movie keyword here" />
+            <button>Submit Search</button>
+        </div>
+    )
+};
+
+export default Search;
